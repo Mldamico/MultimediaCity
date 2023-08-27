5a40ad9185f0abc902296f473d573b51365869ea
mldamico <matias.lioneldamico@gmail.com>
2023-08-27 19:31:04 -0300

    Agregar archivos de proyecto.

---
+++ MultimediaCityWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultimediaCityWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
